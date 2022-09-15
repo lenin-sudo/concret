ebf89f8cece6d2094e79c6bc190f16f9dfe542b9
lenin-sudo <leninlv86@gmail.com>
2022-09-14 21:11:55 -0500

    muestra datos de exel en un treeview

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import tkinter
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
@@ -16,12 +17,18 @@ def centerwindow(ventana, width, higth):
 
 def abrir_archivo():
     # Abre un archivo y muestra la direccion en un Label
+    limpiar_tabla()
     archivo = filedialog.askopenfilename(initialdir='C:\\Users\\lenin\\PycharmProjects\\aceptconcret', title='Seleccionar archivo', filetypes=[('*.xlsx', '*.xlsx'), ('*.xls', '*.xls')])
-    if archivo:
+    criter1 = criterio1.get()
+    criter2 = criterio2.get()
+    if archivo and criter1 and criter2:
         dir_archivo['text'] = archivo
-        pdread.showdata(archivo)
+        df = pdread.showdata(archivo, criter1, criter2)
+        df_rows = df.to_numpy().tolist()
+        for row in df_rows:
+            tabla.insert("", "end", values=row)
     else:
-        dir_archivo['text'] = 'No se ha seleccionado ningun archivo'
+        dir_archivo['text'] = 'No se ha seleccionado ningun archivo. Criterio 1 y Criterio 2 no deben estar vacios'
 
 
 def limpiar_tabla():
@@ -29,13 +36,20 @@ def limpiar_tabla():
 
 
 root = tk.Tk()
-ancho = 600
-alto = 400
+ancho = 710
+alto = 300
 root.geometry("%dx%d" % (ancho, alto))
 root.title('ACI 328-S')
+root.configure(bg='#A2EFFF')
 root.resizable(False, False)
 root.update()
 centerwindow(root, ancho, alto)
+
+# Variables
+criterio1 = tkinter.DoubleVar()
+criterio1.set(24)
+criterio2 = tkinter.DoubleVar()
+criterio2.set(20.5)
 
 # Widgets
 cargar_archivo = tk.Button(root, text='CARGAR', command=abrir_archivo)
@@ -43,22 +57,37 @@ cargar_archivo.grid(row=0, column=0, padx=5, pady=5)
 dir_archivo = tk.Label(root, fg='blue')
 dir_archivo.grid(row=0, column=1, padx=5, pady=5)
 
+tk.Label(root, text='Criterio 1:').grid(row=1, column=0)
+getentryc1 = tk.Entry(root, textvariable=criterio1)
+getentryc1.grid(row=1, column=1, padx=5, pady=5)
+tk.Label(root, text='Criterio 2:').grid(row=1, column=2)
+getentryc2 = tk.Entry(root, textvariable=criterio2)
+getentryc2.grid(row=1, column=3, padx=5, pady=5)
+
 # Tabla
-tabla = ttk.Treeview(root, columns=('#1', '#2', '#3', '#4', '#5'), selectmode='extended', show='headings')
+tabla = ttk.Treeview(root, columns=('#1', '#2', '#3', '#4', '#5', '#6', '#7'), selectmode='extended', show='headings')
+vsb = ttk.Scrollbar(root, orient='vertical', command=tabla.yview)
+vsb.place(x=688, y=70, height=225)
+tabla.configure(yscrollcommand=vsb.set)
 
 # Formateo de columnas
-tabla.column('#1', width=100, minwidth=100, stretch=False)  # tk.NO
+tabla.column('#1', width=120, minwidth=100, stretch=False)  # tk.NO
 tabla.column('#2', width=100, minwidth=100, stretch=False)
 tabla.column('#3', width=100, minwidth=100, stretch=False)
 tabla.column('#4', width=100, minwidth=100, stretch=False)
 tabla.column('#5', width=100, minwidth=100, stretch=False)
+tabla.column('#6', width=80, minwidth=80, stretch=False)
+tabla.column('#7', width=80, minwidth=80, stretch=False)
+
 # Crear encabezado
 tabla.heading('#1', text='Fecha', anchor='center')
 tabla.heading('#2', text='Muestra 1', anchor='center')
 tabla.heading('#3', text='Muestra 2', anchor='center')
 tabla.heading('#4', text='Promedio 1', anchor='center')
 tabla.heading('#5', text='Promedio 2', anchor='center')
-# tabla.insert("", 'end', values=("11:15","Fulanito","¡Hola!"))
-tabla.grid(row=1, column=0, columnspan=2, pady=5, padx=5, sticky='nsew')
+tabla.heading('#6', text='1er Criterio', anchor='center')
+tabla.heading('#7', text='2er Criterio', anchor='center')
+
+tabla.grid(row=2, column=0, columnspan=5, pady=5, padx=5, sticky='nsew')
 
 root.mainloop()
--- pdread.py
+++ pdread.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import xlrd
 import numpy as np
 
 
-def showdata(file):
+def showdata(file, valor1, valor2):
     # https://decodigo.com/python-leer-archivo-de-excel-con-pandas
     # https://youtu.be/Bn1n1diGv_0
     archivo_excel = pd.read_excel(file)
@@ -12,11 +11,63 @@ def showdata(file):
     # print(get_columns)
     # value = archivo_excel['fecha'].values
     # columnas = ['fecha', 'muestra 1 Mpa', 'muestra 2 Mpa']
-    columnas = get_columns  # obtiene nombre de cada columna
+    columnas = get_columns  # obtiene nombre de todas las columna
+    columnas1 = get_columns[1:3]  # obtiene el nombre de ciertas columnas
     show_data = archivo_excel[columnas]  # busca los datos de cada columna
-    print(show_data)
+    # print(show_data)
+    arreglo = show_data[columnas1].to_numpy()
+
+    mat1 = np.zeros((len(arreglo), 1))
+    for i in range(len(arreglo)):
+        suma1 = arreglo[i][0] + arreglo[i][1]
+        promedio1 = suma1/2
+        mat1[i] = promedio1
+
+    mat1 = np.round(mat1, decimals=2)
+    # print('promedio 1:\n', mat1)
+
+    crit1 = np.empty([len(mat1), 1], dtype=str, order='C')
+    for i in range(len(mat1)):
+        if mat1[i] > valor1:
+            crit1[i] = "S"
+        else:
+            crit1[i] = "N"
+
+    # print('crit1:\n', crit1)
+
+    a = 0
+    b = 1
+    c = 2
+    mat2 = np.zeros((len(mat1), 1))
+    while c < len(mat1):
+        suma2 = (mat1[a] + mat1[b] + mat1[c])
+        promedio2 = suma2/3
+        mat2[c] = promedio2
+        a += 1
+        b += 1
+        c += 1
+
+    mat2 = np.round(mat2, decimals=2)
+    # print('promedio 2:\n', mat2)
+
+    crit2 = np.empty([len(mat2), 1], dtype=str, order='C')
+    for i in range(len(mat2)):
+        if mat2[i] > valor2:
+            crit2[i] = "S"
+        elif mat2[i] == 0:
+            crit2[i] = "-"
+        else:
+            crit2[i] = "N"
+
+    # print('crit2:\n', crit2)
+
+    show_data = show_data.assign(prom_1=mat1, prom_2=mat2, crit_1=crit1, crit_2=crit2)  # agrega nuevas columnas
+    return show_data
+    # TODO dar formato a los numeros. Un entero con dos decimales
 
 
-# if __name__ == '__main__':
-# showdata()
+if __name__ == '__main__':
+    data1 = 24
+    data2 = 20.5
+    showdata('C:\\Users\\lenin\\PycharmProjects\\aceptconcret\\Libro1.xlsx', data1, data2)
 
